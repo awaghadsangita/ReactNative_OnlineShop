2bbd20b775329a2ad2d742bbad4dd1c102a23f45
sangita avghad <sangita.awaghad@gmail.com>
2020-06-12 04:23:09 -0700

    ADDED:ActivityIndicator to show loading

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,5 +1,5 @@
-import React,{useEffect} from 'react';
-import {View,FlatList,Text,StyleSheet} from 'react-native';
+import React,{useEffect, useState} from 'react';
+import {View,FlatList,Text,StyleSheet,ActivityIndicator} from 'react-native';
 import {useSelector,useDispatch} from 'react-redux';
 import {HeaderButtons,Item} from 'react-navigation-header-buttons';
 
@@ -7,16 +7,27 @@ import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/OrderItem';
 import * as orderActions from '../../store/action/order';
 const OrderScreen =props=>{
+    const [isLoading,setIsLoading]=useState(false);
     const orders=useSelector(state=>state.orders.orders);
     const dispatch=useDispatch();
     
     useEffect(()=>{
-        dispatch(orderActions.fetchOrder())
+        setIsLoading(true)
+        dispatch(orderActions.fetchOrder()).then(()=>{
+            setIsLoading(false)
+        })
     },[dispatch])
-    if(orders.length==0){
+
+    if(!isLoading && orders.length==0){
         return(<View style={styles.container}><Text style={styles.message}>No Order Placed yet</Text></View>)
     }
-    
+    if(isLoading){
+        return(
+            <View style={styles.center}>
+                <ActivityIndicator size='large' color="#4a0803"/>
+            </View>
+        )
+    }
     return(
         <FlatList
             data={orders}
