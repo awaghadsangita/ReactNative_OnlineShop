bf2333c6cc00fa014fb168f1f4b10cb12db64525
sangita avghad <sangita.awaghad@gmail.com>
2020-06-12 03:10:21 -0700

    ADDED:code to store orders on server

--- store/action/order.js
+++ store/action/order.js
@@ -1,8 +1,28 @@
 export const ADD_ORDER='ADD_ORDER';
 
 export const addOrder=(cartItem,totalAmount)=>{
-    return{
-        type:ADD_ORDER,
-        orderData:{items:cartItem,amount:totalAmount}
-    };
+    return async dispatch=>{
+        const date=new Date();
+        const response= await fetch("https://recipewebsite-95fbf.firebaseio.com/Orders.json",{
+            method:'POST',
+            header:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({
+                cartItem,
+                totalAmount,
+                date
+            })
+        })
+        const resData=await response.json();
+        dispatch({
+            type:ADD_ORDER,
+            orderData:{
+                id:resData.name,
+                items:cartItem,
+                amount:totalAmount,
+                date:date
+            }
+        })
+    }
 }
